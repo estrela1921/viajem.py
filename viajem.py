70ab23d82dcfa920015ce34c3c35ac52654f68a5
estrela1921 <143469066+estrela1921@users.noreply.github.com>
2023-10-24 23:00:13 -0300

    Update viajem.py
    nova configuração utilizando class

--- viajem.py
+++ viajem.py
@@ -1,32 +1,70 @@
 import csv
+from datetime import datetime
 
-# Defina as perguntas e as opções de resposta
-perguntas = [
-    "1. Você gosta de viajar? (1 - Sim, 2 - Não, 3 - Não sei responder)",
-    "2. Você já viajou para outro país? (1 - Sim, 2 - Não, 3 - Não sei responder)",
-    "3. Você costuma viajar em família? (1 - Sim, 2 - Não, 3 - Não sei responder)",
-    "4. Você se planeja para fazer uma viajem? (1 - Sim, 2 - Não, 3 - Não sei responder)"
-]
+class PesquisaViagem:
+    def __init__(self, arquivo_csv):
 
-# Nome do arquivo .csv para armazenar os dados
-arquivo_csv = "dados_pesquisa.csv"
-# Abra o arquivo .csv para escrita
-with open(arquivo_csv, mode='w', newline='') as file:
-    writer = csv.writer(file)
-    writer.writerow(["Idade", "Gênero", "Resposta_1", "Resposta_2", "Resposta_3", "Resposta_4", "Data e Hora"])
+        self.arquivo_csv = arquivo_csv
+        
+        self.perguntas = [
+            "1. Você gosta de viajar? (1 - Sim, 2 - Não, 3 - Não sei responder)",
 
-    while True:
-        print("Bem vindo a nossa Pesquisa sobre viajem")
-        idade = input("  Para começarmos nos Informe sua idade (ou digite '00' para encerrar): ")
-        if idade == '00':
-            break
+            "2. Você já viajou para outro país? (1 - Sim, 2 - Não, 3 - Não sei responder)",
 
-        genero = input("Informe seu gênero: ")
-        respostas = []
+            "3. Você costuma viajar em família? (1 - Sim, 2 - Não, 3 - Não sei responder)",
 
-        for pergunta in perguntas:
-            resposta = input(pergunta)
-            respostas.append(resposta)
+            "4. Você se planeja para fazer uma viagem? (1 - Sim, 2 - Não, 3 - Não sei responder)",
 
-        # Registre a data e hora da resposta
-        ''''writer.writerow([idade, genero] + respostas + [data_hora]'''
+            "5. Quando você viaja você gosta de lugares com praia? (1 - Sim, 2 - Não, 3 - Não sei responder)",
+
+            "6. Você gosta de viajar para lugares com cachoeira? ( 1 - Sim, 2 - Não, 3 - Não sei responder)"
+        ]
+
+    def realizar_pesquisa(self):
+        # Abrindo o arquivo CSV para escrita
+        with open(self.arquivo_csv, mode='w', newline='') as file:
+            writer = csv.writer(file)
+            writer.writerow(["Idade", "Gênero"] + [f"Resposta_{i+1}" for i in range(len(self.perguntas))] + ["Data e Hora"])
+
+            while True:
+                print("Bem-vindo à nossa Pesquisa sobre viagem")
+                idade = input("Para começarmos, informe sua idade (ou digite '00' para encerrar): ")
+                if idade == '00':
+                    break
+
+                genero = input("Informe seu gênero: ")
+                respostas = []
+
+                for pergunta in self.perguntas:
+                    resposta = input(pergunta)
+                    respostas.append(resposta)
+
+                data_hora = datetime.now().strftime("%d-%m-%y %H:%M:%S")
+                writer.writerow([idade, genero] + respostas + [data_hora])
+
+    def exibir_dados_pesquisa(self):
+        # Abrindo o arquivo CSV para leitura
+        with open(self.arquivo_csv, mode='r', newline='') as file:
+            reader = csv.reader(file)
+            header = next(reader)
+            print("Cabeçalho do CSV:")
+            print(header)
+            for row in reader:
+                print("Dados da pesquisa:")
+                print("Idade:", row[0])
+                print("Gênero:", row[1])
+                for i, pergunta in enumerate(self.perguntas):
+                    print(pergunta, "Resposta:", row[i + 2])
+                print("Data e Hora:", row[-1])
+                print("\n")
+
+if __name__ == "__main__":
+    arquivo_csv = "dados_pesquisa.csv"
+    pesquisa = PesquisaViagem(arquivo_csv)
+    pesquisa.realizar_pesquisa()
+    pesquisa.exibir_dados_pesquisa()
+
+
+                
+
+        
