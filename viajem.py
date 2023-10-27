efe8f577263e9685eaa48042b72d0e705a7d6f8e
estrela1921 <143469066+estrela1921@users.noreply.github.com>
2023-10-26 22:25:10 -0300

    Update viajem.pyatualização

--- viajem.py
+++ viajem.py
@@ -1,59 +1,55 @@
  import csv
 from datetime import datetime
 
-class PesquisaViagem:
-    def __init__(self, arquivo_csv):
-
-        self.arquivo_csv = arquivo_csv
-        
+class Perguntas:
+    def __init__(self):
         self.perguntas = [
             "\n1. Você gosta de viajar? (1 - Sim, 2 - Não, 3 - Não sei responder)",
-
             "\n2. Você já viajou para outro país? (1 - Sim, 2 - Não, 3 - Não sei responder)",
-
             "\n3. Você costuma viajar em família? (1 - Sim, 2 - Não, 3 - Não sei responder)",
-
             "\n4. Você se planeja para fazer uma viagem? (1 - Sim, 2 - Não, 3 - Não sei responder)",
-
             "\n5. Quando você viaja você gosta de lugares com praia? (1 - Sim, 2 - Não, 3 - Não sei responder)",
-
             "\n6. Você gosta de viajar para lugares com cachoeira? ( 1 - Sim, 2 - Não, 3 - Não sei responder)"
         ]
 
+class PesquisaViagem:
+    def __init__(self, arquivo_csv):
+        self.arquivo_csv = arquivo_csv
+        self.perguntas = Perguntas().perguntas
 
     def realizar_pesquisa(self):
-        # Abrindo o arquivo CSV para escrita
         with open(self.arquivo_csv, mode='w', newline='') as file:
             writer = csv.writer(file)
-            writer.writerow(["Idade", "Gênero"] + [f"Resposta_{i+1}" for i in range(len(self.perguntas))] + ["Data e Hora"])
+            writer.writerow(["Idade", "Gênero"] + [f"Resposta_{i+1}" for i in range(len(self.perguntas))] + ["Data", "Hora"])
 
             while True:
                 print("\nBem-vindo à nossa Pesquisa sobre viagem")
                 idade = input("\nPara começarmos, informe sua idade (ou digite '00' para encerrar): ")
                 if idade == '00':
-                    break            
-    
+                    break
 
                 genero = input("\nInforme seu gênero: ")
                 respostas = []
 
                 for pergunta in self.perguntas:
-                    resposta = input(pergunta)
-                    respostas.append(resposta)
-                    if not resposta in ["1","2","3"]: 
-                        print("Resposta Inválida. Digite uma opção válida")   
+                    while True:
                         resposta = input(pergunta)
-                        
-                             
-                    data_hora = datetime.now().strftime("\n%d-%m-%y %H:%M:%S")
-                writer.writerow([idade, genero] + respostas + [data_hora])
+                        if resposta in ["1", "2", "3"]:
+                            break
+                        else:
+                            print("Resposta Inválida. Digite uma opção válida (1, 2 ou 3)")
+
+                    data_hora = datetime.now()
+                    data = data_hora.strftime("%d-%m-%Y")
+                    hora = data_hora.strftime("%H:%M:%S")
+                    respostas.append(resposta)
+
+                writer.writerow([idade, genero] + respostas + [data, hora])
 
     def exibir_dados_pesquisa(self):
-        # Abrindo o arquivo CSV para leitura
         with open(self.arquivo_csv, mode='r', newline='') as file:
             reader = csv.reader(file)
             header = next(reader)
-            print("\nCabeçalho do CSV:")
             print(header)
             for row in reader:
                 print("\nDados da pesquisa:")
@@ -61,8 +57,11 @@ class PesquisaViagem:
                 print("\nGênero:", row[1])
                 for i, pergunta in enumerate(self.perguntas):
                     print(pergunta, "\nResposta:", row[i + 2])
-                print("\nData e Hora:", row[-1])
-                print("\n")
+                data = row[-2]  # Índice da coluna de data
+                hora = row[-1]  # Índice da coluna de hora
+                print("\nData:", data)
+                print("Hora:", hora)
+                #print("\n")
 
 if __name__ == "__main__":
     arquivo_csv = "dados_pesquisa.csv"
@@ -70,7 +69,4 @@ if __name__ == "__main__":
     pesquisa.realizar_pesquisa()
     pesquisa.exibir_dados_pesquisa()
 
-
-                
-
-        
+    print("Obrigado por Participar de nossa pesquisa!")
